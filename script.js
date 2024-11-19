88ae71d35a53b09d4a0a2ad36863f9cca257ef72
WissamDouskary <wissamdouskary99@gmail.com>
2024-11-19 12:47:33 +0100

    Configure del button function succes

--- script.js
+++ script.js
@@ -5,22 +5,31 @@ let prioretySelect = document.getElementById("pSelection");
 let statueSelector = document.getElementById("status");
 let myForm = document.getElementById("form");
 
-let taskArr = [];
+let toDoTask = document.getElementById("todo-tasks");
+let inProgressTask = document.getElementById("inProgress-tasks");
+let doneTask = document.getElementById("done-tasks");
+
+
+var taskArr = [];
 var taskObj = {};
+
+getDataFromLocalStorage();
+
 
 myForm.addEventListener("submit", function(event){
 
-    event.preventDefault();
+    event.preventDefault()
 
     if(taskTitle.value !== "" &&
         taskDate.value !== "" &&
         taskDesc.value !== "" &&
         prioretySelect.value !== "Pvide" &&
-        statueSelector.value !== "statusprogress"){
-
+        statueSelector.value !== "statusprogress"
+    ){
         pushToArray(taskTitle, taskDate , taskDesc, prioretySelect, statueSelector);
+        addDataToLocalStorage()
+        taskAdding(taskArr);
         modalClose();
-
     }else{
         alert("please fill all the fields");
         modalOpen();
@@ -42,10 +51,73 @@ function pushToArray(title, date , desc, priorety, status){
         stat: status.value,
     }
     taskArr.push(taskObj);
+    
+}
 
-    let taskArrString = JSON.stringify(taskArr);
-    localStorage.setItem("arr",taskArrString);
+function taskAdding(taskArr){
 
+    toDoTask.innerHTML = "";
+    inProgressTask.innerHTML = "";
+    doneTask.innerHTML = "";
+    
+    
+    
+    taskArr.forEach((taskObj, index) => {
+        
+        let div = document.createElement("div");
+        div.classList = "bg-blue-400 p-3 mt-3"
+
+        div.innerHTML += `<h2>${taskObj.tasktitle}</h2>
+                        <p>${taskObj.description}</p>
+                        <p>${taskObj.dateVal}</p>
+                        <div class="flex gap-3 justify-end">
+                            <button id="editBtn" class="bg-green-400 py-2 px-4 rounded-xl">EDIT</button>
+                            <button id="delBtn" class="bg-red-400 py-2 px-4 rounded-xl">DELETE</button>
+                        </div>`
+        
+        if(taskObj.stat === "todo"){
+            toDoTask.appendChild(div);
+        } else if(taskObj.stat === "doing"){
+            inProgressTask.appendChild(div);
+        } else if(taskObj.stat === "done"){
+            doneTask.appendChild(div);
+        }
+
+
+        if(taskObj.pr === "priorety 1"){
+            div.style.backgroundColor = 'rgb(252 165 165)';
+        } else if (taskObj.pr === "priorety 2"){
+            div.style.backgroundColor = 'rgb(253 224 71)';
+        } else if (taskObj.pr === "priorety 3"){
+            div.style.backgroundColor = '#81C784';
+        }else{
+            alert("please enter a valid priorety !");
+            taskObj.pr = "";
+        }
+
+        let deleteButton = div.querySelector("#delBtn");
+        
+        deleteButton.addEventListener("click", () => {
+        const isConfirm = confirm("Are you sure ?");
+
+        if(isConfirm){
+            taskArr.splice(index, 1);
+            taskAdding(taskArr);
+            addDataToLocalStorage();
+        }
+    });
+    
+
+
+
+        
+
+    });
+
+    
+    
+
+    
 }
 
 
@@ -85,10 +157,7 @@ function pushToArray(title, date , desc, priorety, status){
 
 
 
-
-
-
-
+// modal functions 
 function modalClose(){
     document.getElementById('mainmodal').style.display = 'none'
 }
@@ -96,3 +165,17 @@ function modalOpen(){
     document.getElementById('mainmodal').style.display = 'flex'
 }
 
+
+//local storage functions 
+function addDataToLocalStorage(){
+    let taskArrString = JSON.stringify(taskArr);
+    window.localStorage.setItem("arr",taskArrString);
+}
+function getDataFromLocalStorage(){
+    let dataFromLocal = localStorage.getItem("arr");
+    if(dataFromLocal){
+        let tasks = JSON.parse(dataFromLocal);
+        taskArr = tasks;
+        taskAdding(taskArr);
+    }
+}
